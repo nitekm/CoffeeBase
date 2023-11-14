3e70ff3ef546b66a218fa88871c8e2f510621eaa
ncode <developerncode@gmail.com>
2023-11-14 23:50:38 +0100

    CB-114 Brews - adding new Step: general info WiP

--- app/src/main/java/ncodedev/coffeebase/ui/BrewStepGeneralInfo.java
+++ app/src/main/java/ncodedev/coffeebase/ui/BrewStepGeneralInfo.java
@@ -1,68 +1,40 @@
 package ncodedev.coffeebase.ui;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import ncodedev.coffeebase.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrewStepGeneralInfo extends Fragment {
 
-    private GridView brewMethodGrid;
     private TextView brewMethodDisplayTxt;
-    public BrewStepGeneralInfo() {
-        // Required empty public constructor
-    }
+
+    public BrewStepGeneralInfo() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_brew_step_general_info, container, false);
 
-        initViews(view);
-//        setOnClickListenersOnGrid(brewMethodGrid);
-
+        GridView brewMethodGrid = view.findViewById(R.id.methodGrid);
+        brewMethodDisplayTxt = view.findViewById(R.id.methodNameDisplayTxt);
+        brewMethodGrid.setAdapter(new MethodSelector(brewMethodDisplayTxt));
         return view;
     }
 
-    private void initViews(View view) {
-        brewMethodGrid = view.findViewById(R.id.methodGrid);
-        brewMethodGrid.setAdapter(new MethodSelector());
-        brewMethodDisplayTxt = view.findViewById(R.id.methodNameDisplayTxt);
-    }
+    private class MethodSelector extends BaseAdapter {
 
-    private void setOnClickListenersOnGrid(GridView brewMethodGrid) {
-        brewMethodGrid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        MethodSelector adapter = (MethodSelector) brewMethodGrid.getAdapter();
-        //0 - aeropress
-        brewMethodGrid.getChildAt(0).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.aeropress));
-        //1 - auto drip
-        brewMethodGrid.getChildAt(1).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.auto_drip));
-        //2 - chemex
-        brewMethodGrid.getChildAt(2).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.chemex));
-        //3 - espresso
-        brewMethodGrid.getChildAt(3).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.espresso));
-        //4 - french press
-        brewMethodGrid.getChildAt(4).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.french_press));
-        //5 - mokka pot
-        brewMethodGrid.getChildAt(5).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.mokka_pot));
-        //6 - v60
-        brewMethodGrid.getChildAt(6).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.v60));
-    }
-
-    private class MethodSelector extends ArrayAdapter<ImageView> {
+        private TextView brewMethodDisplayTxt;
+        private List<ImageView> methodIcons = new ArrayList<>();
         private int[] items = {
                 R.drawable.ic_aeropress,
                 R.drawable.ic_auto_drip,
@@ -73,8 +45,8 @@ public class BrewStepGeneralInfo extends Fragment {
                 R.drawable.ic_v60,
         };
 
-        public MethodSelector(@NonNull Context context, int resource) {
-            super(context, resource);
+        public MethodSelector(TextView brewMethodDisplayTxt) {
+            this.brewMethodDisplayTxt = brewMethodDisplayTxt;
         }
 
         @Override
@@ -83,18 +55,49 @@ public class BrewStepGeneralInfo extends Fragment {
         }
 
         @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             ImageView imageView;
             if (convertView == null) {
                 imageView = new ImageView(getContext());
-                imageView.setLayoutParams(new GridView.LayoutParams(150, 150));
+                imageView.setLayoutParams(new GridView.LayoutParams(195, 195));
+                imageView.setPadding(10, 10, 10, 10);
                 imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             } else {
                 imageView = (ImageView) convertView;
             }
 
             imageView.setImageResource(items[position]);
+            setOnLickListeners(position, imageView);
+            methodIcons.add(imageView);
             return imageView;
+        }
+
+        private void setOnLickListeners(int position, ImageView imageView) {
+            switch (position) {
+                case 0 -> imageView.setOnClickListener(view -> setIconSelectedBehaviour(imageView, R.string.aeropress));
+                case 1 -> imageView.setOnClickListener(view -> setIconSelectedBehaviour(imageView, R.string.auto_drip));
+                case 2 -> imageView.setOnClickListener(view -> setIconSelectedBehaviour(imageView, R.string.chemex));
+                case 3 -> imageView.setOnClickListener(view -> setIconSelectedBehaviour(imageView, R.string.espresso));
+                case 4 -> imageView.setOnClickListener(view -> setIconSelectedBehaviour(imageView, R.string.french_press));
+                case 5 -> imageView.setOnClickListener(view -> setIconSelectedBehaviour(imageView, R.string.mokka_pot));
+                case 6 -> imageView.setOnClickListener(view -> setIconSelectedBehaviour(imageView, R.string.v60));
+            }
+        }
+
+        private void setIconSelectedBehaviour(ImageView imageView, int textResourceId) {
+            methodIcons.forEach(icon -> icon.setBackgroundResource(0));
+            imageView.setBackgroundResource(R.drawable.roundcorner);
+            brewMethodDisplayTxt.setText(textResourceId);
         }
     }
 }
