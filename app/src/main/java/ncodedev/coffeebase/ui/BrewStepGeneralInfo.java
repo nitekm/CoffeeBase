46de7515792b1b34e26f04439ac94cc71f9e7efa
ncode <developerncode@gmail.com>
2023-11-14 07:14:29 +0100

    CB-114 Brews - adding new Step: general info WiP

--- app/src/main/java/ncodedev/coffeebase/ui/BrewStepGeneralInfo.java
+++ app/src/main/java/ncodedev/coffeebase/ui/BrewStepGeneralInfo.java
@@ -1,17 +1,22 @@
 package ncodedev.coffeebase.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import ncodedev.coffeebase.R;
 
 public class BrewStepGeneralInfo extends Fragment {
 
+    private GridView brewMethodGrid;
+    private TextView brewMethodDisplayTxt;
     public BrewStepGeneralInfo() {
         // Required empty public constructor
     }
@@ -21,35 +26,61 @@ public class BrewStepGeneralInfo extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_brew_step_general_info, container, false);
 
-        //TODO: rename
-        GridView gridView = view.findViewById(R.id.methodGrid);
-        gridView.setAdapter(new MethodSelector());
+        initViews(view);
+//        setOnClickListenersOnGrid(brewMethodGrid);
 
         return view;
     }
 
-    private class MethodSelector extends BaseAdapter {
+    private void initViews(View view) {
+        brewMethodGrid = view.findViewById(R.id.methodGrid);
+        brewMethodGrid.setAdapter(new MethodSelector());
+        brewMethodDisplayTxt = view.findViewById(R.id.methodNameDisplayTxt);
+    }
+
+    private void setOnClickListenersOnGrid(GridView brewMethodGrid) {
+        brewMethodGrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        MethodSelector adapter = (MethodSelector) brewMethodGrid.getAdapter();
+        //0 - aeropress
+        brewMethodGrid.getChildAt(0).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.aeropress));
+        //1 - auto drip
+        brewMethodGrid.getChildAt(1).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.auto_drip));
+        //2 - chemex
+        brewMethodGrid.getChildAt(2).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.chemex));
+        //3 - espresso
+        brewMethodGrid.getChildAt(3).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.espresso));
+        //4 - french press
+        brewMethodGrid.getChildAt(4).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.french_press));
+        //5 - mokka pot
+        brewMethodGrid.getChildAt(5).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.mokka_pot));
+        //6 - v60
+        brewMethodGrid.getChildAt(6).setOnClickListener(view -> brewMethodDisplayTxt.setText(R.string.v60));
+    }
+
+    private class MethodSelector extends ArrayAdapter<ImageView> {
         private int[] items = {
-                R.drawable.ic_account,
-                R.drawable.ic_info,
-                R.drawable.ic_water_drop,
-                R.drawable.ic_water_drop,
-                R.drawable.ic_water_drop,
-                R.drawable.ic_water_drop,
-                R.drawable.ic_water_drop,
-                R.drawable.ic_water_drop
+                R.drawable.ic_aeropress,
+                R.drawable.ic_auto_drip,
+                R.drawable.ic_chemex,
+                R.drawable.ic_espresso,
+                R.drawable.ic_french_press,
+                R.drawable.ic_mokka_pot,
+                R.drawable.ic_v60,
         };
+
+        public MethodSelector(@NonNull Context context, int resource) {
+            super(context, resource);
+        }
 
         @Override
         public int getCount() {
             return items.length;
         }
-
-        @Override
-        public Object getItem(int position) {return null;}
-
-        @Override
-        public long getItemId(int position) {return 0;}
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
