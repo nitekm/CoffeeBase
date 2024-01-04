34a702d2a65a471a1cf29dc569f0c7532ba25f7b
ncode <developerncode@gmail.com>
2024-01-04 22:36:15 +0100

    quick fix to prod version

--- app/src/main/java/ncodedev/coffeebase/utils/PermissionsUtils.java
+++ app/src/main/java/ncodedev/coffeebase/utils/PermissionsUtils.java
@@ -3,6 +3,8 @@ package ncodedev.coffeebase.utils;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import androidx.annotation.RequiresApi;
 import androidx.core.content.ContextCompat;
 
 
@@ -18,5 +20,10 @@ public class PermissionsUtils {
 
     public static boolean checkReadStoragePermission(Context context) {
         return ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    public static boolean checkReadMediaImagesPermission(Context context) {
+        return ContextCompat.checkSelfPermission(context, Manifest.permission.READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED;
     }
 }
